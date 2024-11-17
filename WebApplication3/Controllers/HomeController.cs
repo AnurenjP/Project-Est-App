36b477a4cce3a2cde672757b0ac85f47bd3be279
AnurenjP <Anurenj.P@cognizant.com>
2024-11-17 16:25:49 +0530

    updates: sample budget -auto updation adn data traversal project name

--- WebApplication3/Controllers/HomeController.cs
+++ WebApplication3/Controllers/HomeController.cs
@@ -6,17 +6,6 @@ namespace ProjectEstimationApp.Controllers
 {
     public class HomeController : Controller
     {
-        [HttpPost]
-        public IActionResult SubmitForm(string projectType, string complexity, int hours)
-        {
-            // Store the form data in TempData to pass it to the next page
-            TempData["ProjectType"] = projectType;
-            TempData["Complexity"] = complexity;
-            TempData["Hours"] = hours;
-
-            // Redirect to the next page
-            return RedirectToAction("Index");
-        }
         private readonly ILogger<HomeController> _logger;
 
         public HomeController(ILogger<HomeController> logger)
@@ -24,35 +13,51 @@ namespace ProjectEstimationApp.Controllers
             _logger = logger;
         }
 
-        public ActionResult Index()
-        {
-            return View();
-        }
-        public ActionResult Index2()
-        {
-            return View();
-        }
-        public ActionResult Index3()
-        {
-            return View();
-        }
-        public ActionResult smbud()
+        public IActionResult Index()
         {
             return View();
         }
 
+        [HttpPost]
+        public IActionResult SubmitForm(ProjectEstimation model)
+        {
+            // Store the form data in TempData to pass it to the next page
+            TempData["ProjectEstimation"] = model;
 
+            // Redirect to the next page
+            return RedirectToAction("Estpage");
+        }
 
-        public ActionResult Privacy()
+        public IActionResult Estpage()
+        {
+            var model = TempData["ProjectEstimation"] as ProjectEstimation;
+            return View(model);
+        }
+
+        public IActionResult Resource()
         {
             return View();
         }
 
-        public ActionResult Homepage()
+        public IActionResult Sampletimeline()
         {
             return View();
         }
 
+        public IActionResult smbud()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult Homepage()
+        {
+            return View();
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
---
+++ WebApplication3/Models/ProjectEstimation.cs
@@ -0,0 +1,10 @@
+﻿namespace ProjectEstimationApp.Models
+{
+    public class ProjectEstimation
+    {
+        public string? ProjectName { get; set; }
+        public string? ProjectType { get; set; }
+        public string? Complexity { get; set; }
+        public int Hours { get; set; }
+    }
+}
