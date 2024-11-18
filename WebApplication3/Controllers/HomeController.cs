251c88bf5abf4087e5a5a17abff694b3b356fd27
AnurenjP <Anurenj.P@cognizant.com>
2024-11-18 18:00:43 +0530

    18-11-2024 6 PM updated files changed

--- WebApplication3/Controllers/HomeController.cs
+++ WebApplication3/Controllers/HomeController.cs
@@ -1,6 +1,8 @@
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 using ProjectEstimationApp.Models;
+using OfficeOpenXml;
+using System.IO;
 
 namespace ProjectEstimationApp.Controllers
 {
@@ -44,7 +46,7 @@ namespace ProjectEstimationApp.Controllers
             return View();
         }
 
-        public IActionResult smbud()
+        public IActionResult Smbud()
         {
             return View();
         }
@@ -57,6 +59,75 @@ namespace ProjectEstimationApp.Controllers
         public IActionResult Homepage()
         {
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult GenerateFiles([FromBody] ProjectData projectData)
+        {
+            var excelFilePath = GenerateExcel(projectData);
+            return Json(new { success = true, excelFilePath });
+        }
+
+        private string GenerateExcel(ProjectData projectData)
+        {
+            // Set the license context for EPPlus
+            ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
+
+            var downloadsPath = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
+            var filePath = Path.Combine(downloadsPath, "Downloads", "ProjectEstimation.xlsx");
+
+            try
+            {
+                using (var package = new ExcelPackage())
+                {
+                    var worksheet = package.Workbook.Worksheets.Add("Project Estimation");
+                    worksheet.Cells[1, 1].Value = "Resource Type";
+                    worksheet.Cells[1, 2].Value = "Cost";
+                    worksheet.Cells[1, 3].Value = "Number of Resources";
+                    worksheet.Cells[1, 4].Value = "Total";
+
+                    int row = 2;
+                    foreach (var resource in projectData.Resources)
+                    {
+                        worksheet.Cells[row, 1].Value = resource.Name;
+                        worksheet.Cells[row, 2].Value = resource.Cost;
+                        worksheet.Cells[row, 3].Value = resource.NumberOfResources;
+                        worksheet.Cells[row, 4].Value = resource.Total;
+                        row++;
+                    }
+
+                    worksheet.Cells[row, 1].Value = "Project Start Date";
+                    worksheet.Cells[row, 2].Value = projectData.ProjectStartDate;
+
+                    worksheet.Cells[row + 1, 1].Value = "Project End Date";
+                    worksheet.Cells[row + 1, 2].Value = projectData.ProjectEndDate;
+
+                    var additionalCostsSheet = package.Workbook.Worksheets.Add("Additional Costs");
+                    additionalCostsSheet.Cells[1, 1].Value = "Name";
+                    additionalCostsSheet.Cells[1, 2].Value = "Cost";
+                    additionalCostsSheet.Cells[1, 3].Value = "Number of Resources";
+                    additionalCostsSheet.Cells[1, 4].Value = "Total";
+
+                    int additionalRow = 2;
+                    foreach (var cost in projectData.AdditionalCosts)
+                    {
+                        additionalCostsSheet.Cells[additionalRow, 1].Value = cost.Name;
+                        additionalCostsSheet.Cells[additionalRow, 2].Value = cost.Cost;
+                        additionalCostsSheet.Cells[additionalRow, 3].Value = cost.NumberOfResources;
+                        additionalCostsSheet.Cells[additionalRow, 4].Value = cost.Total;
+                        additionalRow++;
+                    }
+
+                    package.SaveAs(new FileInfo(filePath));
+                }
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Error saving Excel file");
+                throw;
+            }
+
+            return filePath;
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
---
+++ WebApplication3/Models/ProjectData.cs
@@ -0,0 +1,26 @@
+﻿namespace ProjectEstimationApp.Models
+{
+    public class ProjectData
+    {
+        public List<Resource> Resources { get; set; } = new List<Resource>();
+        public string? ProjectStartDate { get; set; }
+        public string? ProjectEndDate { get; set; }
+        public List<AdditionalCost> AdditionalCosts { get; set; } = new List<AdditionalCost>();
+    }
+
+    public class Resource
+    {
+        public string Name { get; set; } = string.Empty;
+        public float Cost { get; set; }
+        public int NumberOfResources { get; set; }
+        public float Total => Cost * NumberOfResources;
+    }
+
+    public class AdditionalCost
+    {
+        public string Name { get; set; } = string.Empty;
+        public float Cost { get; set; }
+        public int NumberOfResources { get; set; }
+        public float Total => Cost * NumberOfResources;
+    }
+}
